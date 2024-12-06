ec70416d554d8954c36fbb10ae86e4e788ec0367
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-12-06 10:56:30 -0800

    Adding logic for faux wood blind cuts

--- src/components/FauxWood/FauxWood.jsx
+++ src/components/FauxWood/FauxWood.jsx
@@ -1,14 +1,75 @@
-import React from "react";
-import './faux-wood.css'
+import React, { useState } from "react";
+import "./faux-wood.css";
 
 export default function FauxWood() {
-    let Ogblinds = 0;
-    let newBlinds = 0;
+  const [OriginalValue, setOriginalValue] = useState("");
+  const [DesiredValue, setDesiredValue] = useState("");
+  const [Cut, setCut] = useState("");
 
-    return (
-        <div>
-            <h1>Faux Wood Blinds</h1>
-            <h2>Please enter your measurements</h2>
-        </div>
-    )
+  const handleOriginalInput = (event) => {
+    const OriginalValue = event.target.value;
+    const regex = /^\s*(\d+)?(\s+(\d+\/?\d*)?)?$/;
+
+    if (regex.test(OriginalValue) || OriginalValue === "") {
+      setOriginalValue(OriginalValue);
+    }
+  };
+
+  const handleDesiredInput = (event) => {
+    const DesiredValue = event.target.value;
+    const regex = /^\s*(\d+)?(\s+(\d+\/?\d*)?)?$/;
+
+    if (regex.test(DesiredValue) || DesiredValue === "") {
+      setDesiredValue(DesiredValue);
+    }
+  };
+
+  const handleCut = (event) => {
+    event.preventDefault();
+    let blindWidth = OriginalValue;
+    let blindCut = DesiredValue;
+    console.log(blindWidth);
+    console.log(blindCut);
+    // setCut();
+    // console.log()
+  };
+
+  return (
+    <div>
+      <h1>Faux Wood Blinds</h1>
+      <h2>Please enter your measurements</h2>
+      <form onSubmit={handleCut}>
+        <fieldset>
+          <label>
+            Enter the width of your current blind
+            <input
+              type="text"
+              value={OriginalValue}
+              onChange={handleOriginalInput}
+              placeholder="Example: 30 1/2"
+            />
+          </label>
+          <label>
+            Enter the desired width of your blind
+            <input
+              type="test"
+              value={DesiredValue}
+              onChange={handleDesiredInput}
+              placeholder="Example: 28 1/2"
+            />
+          </label>
+          <input 
+            type="submit"
+            value="Submit"
+          />
+        </fieldset>
+      </form>
+      <div className="cut-showcase">
+        <h2>Display Output here</h2>
+      </div>
+      <div>
+        Add hidden tape measure picture to help people who cant read one
+      </div>
+    </div>
+  );
 }
