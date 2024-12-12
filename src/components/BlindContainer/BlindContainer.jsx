bc3f12948f7b72d19035c3fe6e0247e87a9bc85b
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-12-12 14:56:01 -0800

    Styling and layout changes

--- src/components/BlindContainer/BlindContainer.jsx
+++ src/components/BlindContainer/BlindContainer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import FauxWoodBlindsImage from "../../images/fauxwoodblinds.png";
+import CellularShadeImage from "../../images/cellularshadeimageedited.png"
 import "./blind-container.css";
 
 export default function BlindContainer() {
@@ -8,26 +10,32 @@ export default function BlindContainer() {
   return (
     <div className="blind-container-container">
       <div className="faux-wood-cotainer">
-        <button
-          className="faux-wood-button"
-          onClick={() => {
-            console.log("redirecting...");
-            navigate("/fauxWood");
-          }}
-        >
-          Navigate to Faux Wood Page
+        <button className="faux-wood-button">
+          <img
+            className="faux-wood-image"
+            src={FauxWoodBlindsImage}
+            alt="Closed shutter designed by Freepik"
+            onClick={() => {
+              console.log("redirecting...");
+              navigate("/fauxWood");
+            }}
+          />
+          <p>Photo Desgined by Freepik</p>
         </button>
         <h1>Faux Wood</h1>
       </div>
       <div className="cellular-container">
-        <button
-          className="cellular-button"
+        <button className="cellular-button">
+          <img
+            className="cellular-shade-image"
+            src={CellularShadeImage}
+            alt="Window Roller Shutters Set designed by Freepik"
           onClick={() => {
             console.log("redirecting...");
             navigate("/cellular");
           }}
-        >
-          Navigate to Cellular Shade Page
+          />
+          <p>Photo designed by Freepik</p>
         </button>
         <h1>Cellular Shade</h1>
       </div>
