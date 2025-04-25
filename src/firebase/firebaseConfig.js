de496b1f5567902a6e0a278b926a4886414e633a
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2025-04-24 20:46:20 -0700

    Remove .env file from Git tracking

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -8,7 +8,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyANFXWLoeYAqFgPfG8XBqcwUrJvj0y8bH0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "exact-blinds.firebaseapp.com",
   projectId: "exact-blinds",
   storageBucket: "exact-blinds.firebasestorage.app",
