9cc563f0442356755099ad16dc431b5c56d6cd14
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2025-06-03 19:57:44 -0700

    Mobile styling for home page

--- frontend/src/components/BlindContainer/BlindContainer.jsx
+++ frontend/src/components/BlindContainer/BlindContainer.jsx
@@ -13,42 +13,48 @@ export default function BlindContainer() {
       <div className="blind-container-faux-wood">
         <h1>Faux Wood</h1>
         <button className="faux-wood-button">
-          <img
-            className="faux-wood-image"
-            src={FauxWoodBlindsImage}
-            alt="Closed shutter designed by Freepik"
-            onClick={() => {
-              navigate("/fauxWood");
-            }}
-          />
+          <div className="blind-image-wrapper">
+            <img
+              className="faux-wood-image"
+              src={FauxWoodBlindsImage}
+              alt="Closed shutter designed by Freepik"
+              onClick={() => {
+                navigate("/fauxWood");
+              }}
+            />
+          </div>
           <p>Photo Desgined by Freepik</p>
         </button>
       </div>
       <div className="blind-container-cellular">
         <h1>Cellular Shade</h1>
         <button className="cellular-button">
-          <img
-            className="cellular-shade-image"
-            src={CellularShadeImage}
-            alt="Window Roller Shutters Set designed by Freepik"
-            onClick={() => {
-              navigate("/cellular");
-            }}
-          />
+          <div className="blind-image-wrapper">
+            <img
+              className="cellular-shade-image"
+              src={CellularShadeImage}
+              alt="Window Roller Shutters Set designed by Freepik"
+              onClick={() => {
+                navigate("/cellular");
+              }}
+            />
+          </div>
           <p>Photo designed by Freepik</p>
         </button>
       </div>
       <div className="blind-container-vinyl">
         <h1>Vinyl</h1>
         <button className="vinyl-button">
-          <img
-            className="vinyl-blind-image"
-            src={VinylBlindsImage}
-            alt="Office desk still life designed by Freepik"
-            onClick={() => {
-              navigate("/vinyl");
-            }}
-          />
+          <div className="blind-image-wrapper">
+            <img
+              className="vinyl-blind-image"
+              src={VinylBlindsImage}
+              alt="Office desk still life designed by Freepik"
+              onClick={() => {
+                navigate("/vinyl");
+              }}
+            />
+          </div>
           <p>Photo designed by Freepik</p>
         </button>
       </div>
